41b3cd2d013ef88828075268307421c180bb94db
Milos Bijelic <mbijelic.03@gmail.com>
2017-12-02 04:37:31 -0500

    Improved daily reset conditional

--- water_vis_app_skeleton.py
+++ water_vis_app_skeleton.py
@@ -1,17 +1,15 @@
-import datetime 
-from threading import Thread
+import time 
 
 def calc_water_goal(): 
 	# Calculate the water goal
 	# return water_goal
 
-# If a water value is imported into the GUI
+# If a water value is entered into the GUI, store value
 def water_input(ml):
-    water += ml 
-    return water 
+    current_intake += ml 
+    return current_intake 
 
 # Changes the puffer fish image depending on water intake
-# final_val = water goal, water = current daily water intake
 def fish_change(water_goal, current_intake): 
 	if(0.33*water_goal >= current_intake >= 0): 
 		#use fish image 1
@@ -20,23 +18,29 @@ def fish_change(water_goal, current_intake):
 	else(): 
 		#use fish image 3
 
-# Checks if it is a new day 
+# resets counters and pushes final value on day change
 def daily_reset(): 
-current_time = strftime("%H:%M:%S")
-if (current_time == "00:00:00"): 
 	#push water counter to array 
-	#reset water counter
+	current_intake = 0
 
+# Line below means "ONLY run this code below when I call the current file"
+# If you include this file in another script the code below will not run
 if __name__ == "__main__":
 
-    do calc_water_goal() while True: 
-    
-        if( //a GUI value is entered// ): 
+    calc_water_goal()
+    current_day=strftime("%d")
+    current_intake = 0
+    while True: 
+    	# activates if a value is passed in from the GUI
+        if ( //a GUI value is entered// ): 
         	water_input(val_from_GUI)
-        	fish_change(water_goal, val_from_GUI)
+        	fish_change(water_goal, current_intake)
 
-        if(time.now == "00:00:00")
+        # activates if the date does not match the one 
+        # initialized in the beginning of the script
+        if(current_day != strftime("%d"))
             daily_reset():
+            current_day = strftime("%d")
         else(): 
         	continue
 
